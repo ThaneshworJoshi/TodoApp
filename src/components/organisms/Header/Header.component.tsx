285a8d25dd17d3fa1240b12b21758c111a3615be
Thaneshwor Joshi <thaneshwor10@gmail.com>
2023-08-20 21:58:49 +0545

    feat(component): create Header component

---
+++ src/components/organisms/Header/Header.component.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Box, Container, styled, Typography } from '@mui/material'
+
+const StyledHeaderWrapper = styled(Box)(() => {
+  return {
+    padding: '20px 0',
+    backgroundColor: '#76CC8E',
+  }
+})
+export const Header = () => {
+  return (
+    <StyledHeaderWrapper>
+      <Container maxWidth="lg">
+        <Typography variant="h4" component="h1" fontWeight="bold">
+          Todo App
+        </Typography>
+      </Container>
+    </StyledHeaderWrapper>
+  )
+}
+
+export default Header
